51cc02eb417976bd69a365fa803801f2d4a93fb0
plout <1025924298@qq.com>
2025-09-30 16:28:39 +0800

    初始化

---
+++ src/main/java/com/zhou/train/TestController.java
@@ -0,0 +1,12 @@
+package com.zhou.train;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @RequestMapping("/hello")
+    public String test() {
+        return "hello world1";
+    }
+}
